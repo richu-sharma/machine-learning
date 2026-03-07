052467e7917f4b070eab2d393fb7e8da4bc71a5d
richu-sharma <149307203+richu-sharma@users.noreply.github.com>
2026-03-07 23:03:43 +0530

    Create kmeans_clustering.py

---
+++ unsupervised_learning/k_means_clustering/kmeans_clustering.py
@@ -0,0 +1,15 @@
+# Clustering Algorithms
+
+This folder contains implementations of **unsupervised clustering algorithms** used to group similar data points together.
+
+Algorithms included:
+
+* K-Means Clustering
+* Hierarchical Clustering
+* DBSCAN
+
+Clustering is commonly used in:
+
+* Customer segmentation
+* Market analysis
+* Pattern recognition
